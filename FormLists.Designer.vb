d93a2934ba9344d5f5178c81ad55e3c5d21373ae
Pedro David Polonia <polonia2@gmail.com>
2019-05-13 10:44:50 +0200

    Commit to github
    all files from yoga 920

---
+++ FormLists.Designer.vb
@@ -0,0 +1,168 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class FormLists
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.ListBox2 = New System.Windows.Forms.ListBox()
+        Me.ButtonReset = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.Panel1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.GroupBox1.Controls.Add(Me.ListBox1)
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 15)
+        Me.GroupBox1.MinimumSize = New System.Drawing.Size(80, 276)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(327, 276)
+        Me.GroupBox1.TabIndex = 0
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "GroupBox1"
+        '
+        'ListBox1
+        '
+        Me.ListBox1.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.Location = New System.Drawing.Point(14, 26)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(294, 225)
+        Me.ListBox1.TabIndex = 0
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.GroupBox2.Controls.Add(Me.ListBox2)
+        Me.GroupBox2.Location = New System.Drawing.Point(429, 21)
+        Me.GroupBox2.MinimumSize = New System.Drawing.Size(80, 269)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(359, 269)
+        Me.GroupBox2.TabIndex = 1
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "GroupBox2"
+        '
+        'ListBox2
+        '
+        Me.ListBox2.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.ListBox2.FormattingEnabled = True
+        Me.ListBox2.Location = New System.Drawing.Point(17, 33)
+        Me.ListBox2.Name = "ListBox2"
+        Me.ListBox2.Size = New System.Drawing.Size(324, 199)
+        Me.ListBox2.TabIndex = 0
+        '
+        'ButtonReset
+        '
+        Me.ButtonReset.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.ButtonReset.Location = New System.Drawing.Point(18, 57)
+        Me.ButtonReset.Name = "ButtonReset"
+        Me.ButtonReset.Size = New System.Drawing.Size(54, 33)
+        Me.ButtonReset.TabIndex = 2
+        Me.ButtonReset.Text = "Reset"
+        Me.ButtonReset.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Button2.Location = New System.Drawing.Point(18, 111)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(49, 29)
+        Me.Button2.TabIndex = 3
+        Me.Button2.Text = "Button2"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Button3.Location = New System.Drawing.Point(18, 149)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(51, 49)
+        Me.Button3.TabIndex = 4
+        Me.Button3.Text = "Button3"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Panel1
+        '
+        Me.Panel1.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Panel1.Controls.Add(Me.Button1)
+        Me.Panel1.Controls.Add(Me.Button2)
+        Me.Panel1.Controls.Add(Me.Button3)
+        Me.Panel1.Controls.Add(Me.ButtonReset)
+        Me.Panel1.Location = New System.Drawing.Point(345, 21)
+        Me.Panel1.MinimumSize = New System.Drawing.Size(78, 270)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(78, 270)
+        Me.Panel1.TabIndex = 5
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(18, 210)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(48, 34)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'FormLists
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.Panel1)
+        Me.MinimumSize = New System.Drawing.Size(616, 389)
+        Me.Name = "FormLists"
+        Me.Text = "Form List Re-size"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox2.ResumeLayout(False)
+        Me.Panel1.ResumeLayout(False)
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents ListBox2 As ListBox
+    Friend WithEvents ButtonReset As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents Button1 As Button
+End Class
---
+++ FormLists.vb
@@ -0,0 +1,95 @@
+﻿Public Class FormLists
+
+#Region "Panel UX resize"
+
+    ''' <summary>
+    ''' Panel1_MouseLeave - Small Panel area for resize - MouseLeave event
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    Private Sub Panel1_MouseLeave(sender As Object, e As EventArgs) Handles Panel1.MouseLeave
+        ' mouse feedback on leave
+        Cursor.Current = System.Windows.Forms.Cursors.Default
+    End Sub
+
+    ''' <summary>
+    ''' Panel1_MouseMove - Small Panel area for resize - MouseMove event
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    Private Sub Panel1_MouseMove(sender As Object, e As MouseEventArgs) Handles Panel1.MouseMove
+        If e.Button.Equals(MouseButtons.Left) Then
+            MovePanel(CInt(PosX - MousePosition.X))
+        Else
+            ' reset position and cursor
+            PosX = 0
+            Cursor.Current = System.Windows.Forms.Cursors.Default
+        End If
+    End Sub
+
+    ''' <summary>
+    ''' Last mouse X position
+    ''' </summary>
+    Private PosX As Double
+
+    ''' <summary>
+    ''' MinGroupSize - Low Group Size limit (always large than this value)
+    ''' </summary>
+    Const MinGroupSize As Integer = 100
+
+    ''' <summary>
+    ''' MovePanel - Move horizontaly the panels by DeltaX
+    ''' </summary>
+    ''' <param name="DeltaX">Delta X</param>
+    Private Sub MovePanel(ByVal DeltaX As Integer)
+        Cursor.Current = System.Windows.Forms.Cursors.SizeWE
+
+        ' Initial Pos
+        If PosX.Equals(0) Then
+            ' initial status
+            PosX = MousePosition.X '+ Panel1.Left
+            DeltaX = 0
+        End If
+        ' Min size
+        If (GroupBox1.Width <= MinGroupSize) And (DeltaX > 0) Then
+            DeltaX = 0
+        End If
+        If (GroupBox2.Width <= MinGroupSize) And (DeltaX < 0) Then
+            DeltaX = 0
+        End If
+
+        ' Move
+        If Not DeltaX.Equals(0) Then
+            GroupBox1.Width -= DeltaX
+            Panel1.Left -= DeltaX
+            GroupBox2.Left -= DeltaX
+            GroupBox2.Width += DeltaX
+
+            ' update new position
+            PosX = MousePosition.X
+        End If
+    End Sub
+
+    ''' <summary>
+    ''' ResetPanel - Resize the panels positions, with half of total size for each
+    ''' </summary>
+    Private Sub ResetPanel()
+        Dim TotalGroupSize As Integer
+
+        TotalGroupSize = GroupBox1.Width + GroupBox2.Width
+
+        GroupBox1.Width = CInt(TotalGroupSize / 2)
+        GroupBox2.Width = CInt(TotalGroupSize / 2)
+        Panel1.Left = GroupBox1.Left + GroupBox1.Width + 10
+        GroupBox2.Left = Panel1.Left + Panel1.Width + 10
+        PosX = 0
+    End Sub
+
+#End Region
+
+    Private Sub ButtonReset_Click(sender As Object, e As EventArgs) Handles ButtonReset.Click
+        ResetPanel()
+    End Sub
+
+
+End Class
